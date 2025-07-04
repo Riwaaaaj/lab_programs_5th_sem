d8f713db7ea4ee2120d5cf699ada6043b373ebc4
Riwaaaaj <riwajkarki57@gmail.com>
2025-07-04 05:53:15 +0545

    CG Lab 3 completed

--- CG/lab 3/MPCIRCLE.C
+++ CG/lab 3/MPCIRCLE.C
@@ -2,31 +2,30 @@
 #include <stdio.h>
 #include <conio.h>
 
-void drawCircle(int xc, int yc, int r) {
+void drawCircle(int a, int b, int r) {
     int x = 0;
     int y = r;
     float p = 5 / 4 - r;
 
     while (x <= y) {
-	// Plot the 8 symmetric points
-	putpixel(xc + x, yc + y, WHITE);
-	putpixel(xc + y, yc + x, RED);
-	putpixel(xc + y, yc - x, WHITE);
-	putpixel(xc + x, yc - y, RED);
-	putpixel(xc - x, yc - y, WHITE);
-	putpixel(xc - y, yc - x, RED);
-	putpixel(xc - y, yc + x, WHITE);
-	putpixel(xc - x, yc + y, RED);
 
-	x++;
+		putpixel(a + x, b + y, BLUE);
+		putpixel(a + y, b + x, RED);
+		putpixel(a + y, b - x, BLUE);
+		putpixel(a + x, b - y, RED);
+		putpixel(a - x, b - y, BLUE);
+		putpixel(a - y, b - x, RED);
+		putpixel(a - y, b + x, BLUE);
+		putpixel(a - x, b + y, RED);
 
-	if (p < 0)
-	    p = p + 2 * x + 1;
-	else {
-	    y--;
-	    p = p + 2 * (x - y) + 1;
+		x++;
+		p = p + 2 * x + 1;
+		
+		if (p >= 0) {
+			y--;
+			p = p - 2 *y;
+		}
 	}
-    }
 }
 
 int main() {
@@ -34,7 +33,7 @@ int main() {
     int a, b, r;
 
     initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
-
+	setbkcolor(WHITE);  // 👈 Set background color to white
     printf("Enter center (a, b) and radius: ");
     scanf("%d %d %d", &a, &b, &r);
 
